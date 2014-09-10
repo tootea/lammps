52f62d2765021b7ae6de65eb0a1c60a756cd0f6a
Tomáš Trnka <ttrnka@mail.muni.cz>
2014-09-10 16:53:40 +0200

    Inline funkce rvec_*

--- src/USER-REAXC/reaxc_vector.h
+++ src/USER-REAXC/reaxc_vector.h
@@ -27,38 +27,211 @@
 #ifndef __VECTOR_H_
 #define __VECTOR_H_
 
-#include "pair.h"
 #include "reaxc_types.h"
 #include "reaxc_defs.h"
 
-void rvec_Copy( rvec, rvec );
-void rvec_Scale( rvec, real, rvec );
-void rvec_Add( rvec, rvec );
-void rvec_ScaledAdd( rvec, real, rvec );
-void rvec_Sum( rvec, rvec, rvec );
-void rvec_ScaledSum( rvec, real, rvec, real, rvec );
-real rvec_Dot( rvec, rvec );
-real rvec_ScaledDot( real, rvec, real, rvec );
-void rvec_Multiply( rvec, rvec, rvec );
-void rvec_iMultiply( rvec, ivec, rvec );
-void rvec_Divide( rvec, rvec, rvec );
-void rvec_iDivide( rvec, rvec, ivec );
-void rvec_Invert( rvec, rvec );
-void rvec_Cross( rvec, rvec, rvec );
-void rvec_OuterProduct( rtensor, rvec, rvec );
-real rvec_Norm_Sqr( rvec );
-real rvec_Norm( rvec );
-int  rvec_isZero( rvec );
-void rvec_MakeZero( rvec );
-void rvec_Random( rvec );
+static void rvec_Copy( rvec dest, rvec src )
+{
+  dest[0] = src[0], dest[1] = src[1], dest[2] = src[2];
+}
 
-void rtensor_MakeZero( rtensor );
-void rtensor_MatVec( rvec, rtensor, rvec );
-void rtensor_Scale( rtensor, real, rtensor );
 
-void ivec_MakeZero( ivec );
-void ivec_Copy( ivec, ivec );
-void ivec_Scale( ivec, real, ivec );
-void ivec_Sum( ivec, ivec, ivec );
+static void rvec_Scale( rvec ret, real c, rvec v )
+{
+  ret[0] = c * v[0], ret[1] = c * v[1], ret[2] = c * v[2];
+}
+
+
+static void rvec_Add( rvec ret, rvec v )
+{
+  ret[0] += v[0], ret[1] += v[1], ret[2] += v[2];
+}
+
+
+static void rvec_ScaledAdd( rvec ret, real c, rvec v )
+{
+  ret[0] += c * v[0], ret[1] += c * v[1], ret[2] += c * v[2];
+}
+
+
+static void rvec_Sum( rvec ret, rvec v1 ,rvec v2 )
+{
+  ret[0] = v1[0] + v2[0];
+  ret[1] = v1[1] + v2[1];
+  ret[2] = v1[2] + v2[2];
+}
+
+
+static void rvec_ScaledSum( rvec ret, real c1, rvec v1 ,real c2, rvec v2 )
+{
+  ret[0] = c1 * v1[0] + c2 * v2[0];
+  ret[1] = c1 * v1[1] + c2 * v2[1];
+  ret[2] = c1 * v1[2] + c2 * v2[2];
+}
+
+
+static real rvec_Dot( rvec v1, rvec v2 )
+{
+  return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+}
+
+
+static real rvec_ScaledDot( real c1, rvec v1, real c2, rvec v2 )
+{
+  return (c1*c2) * (v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]);
+}
+
+
+static void rvec_Multiply( rvec r, rvec v1, rvec v2 )
+{
+  r[0] = v1[0] * v2[0];
+  r[1] = v1[1] * v2[1];
+  r[2] = v1[2] * v2[2];
+}
+
+
+static void rvec_iMultiply( rvec r, ivec v1, rvec v2 )
+{
+  r[0] = v1[0] * v2[0];
+  r[1] = v1[1] * v2[1];
+  r[2] = v1[2] * v2[2];
+}
+
+
+static void rvec_Divide( rvec r, rvec v1, rvec v2 )
+{
+  r[0] = v1[0] / v2[0];
+  r[1] = v1[1] / v2[1];
+  r[2] = v1[2] / v2[2];
+}
+
+
+static void rvec_iDivide( rvec r, rvec v1, ivec v2 )
+{
+  r[0] = v1[0] / v2[0];
+  r[1] = v1[1] / v2[1];
+  r[2] = v1[2] / v2[2];
+}
+
+
+static void rvec_Invert( rvec r, rvec v )
+{
+  r[0] = 1. / v[0];
+  r[1] = 1. / v[1];
+  r[2] = 1. / v[2];
+}
+
+
+static void rvec_Cross( rvec ret, rvec v1, rvec v2 )
+{
+  ret[0] = v1[1] * v2[2] - v1[2] * v2[1];
+  ret[1] = v1[2] * v2[0] - v1[0] * v2[2];
+  ret[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+
+static void rvec_OuterProduct( rtensor r, rvec v1, rvec v2 )
+{
+  int i, j;
+
+  for( i = 0; i < 3; ++i )
+    for( j = 0; j < 3; ++j )
+      r[i][j] = v1[i] * v2[j];
+}
+
+
+static real rvec_Norm_Sqr( rvec v )
+{
+  return SQR(v[0]) + SQR(v[1]) + SQR(v[2]);
+}
+
+
+static real rvec_Norm( rvec v )
+{
+  return sqrt( SQR(v[0]) + SQR(v[1]) + SQR(v[2]) );
+}
+
+
+static int rvec_isZero( rvec v )
+{
+  if( fabs(v[0]) > ALMOST_ZERO ||
+      fabs(v[1]) > ALMOST_ZERO ||
+      fabs(v[2]) > ALMOST_ZERO )
+    return 0;
+  return 1;
+}
+
+
+static void rvec_MakeZero( rvec v )
+{
+  v[0] = v[1] = v[2] = 0.000000000000000e+00;
+}
+
+
+static void rtensor_MatVec( rvec ret, rtensor m, rvec v )
+{
+  int i;
+  rvec temp;
+
+  if( ret == v )
+    {
+      for( i = 0; i < 3; ++i )
+        temp[i] = m[i][0] * v[0] + m[i][1] * v[1] + m[i][2] * v[2];
+
+      for( i = 0; i < 3; ++i )
+        ret[i] = temp[i];
+    }
+  else
+    {
+      for( i = 0; i < 3; ++i )
+        ret[i] = m[i][0] * v[0] + m[i][1] * v[1] + m[i][2] * v[2];
+    }
+}
+
+
+static void rtensor_Scale( rtensor ret, real c, rtensor m )
+{
+  int i, j;
+
+  for( i = 0; i < 3; ++i )
+    for( j = 0; j < 3; ++j )
+      ret[i][j] = c * m[i][j];
+}
+
+
+static void rtensor_MakeZero( rtensor t )
+{
+  t[0][0] = t[0][1] = t[0][2] = 0;
+  t[1][0] = t[1][1] = t[1][2] = 0;
+  t[2][0] = t[2][1] = t[2][2] = 0;
+}
+
+
+static void ivec_MakeZero( ivec v )
+{
+  v[0] = v[1] = v[2] = 0;
+}
+
+
+static void ivec_Copy( ivec dest, ivec src )
+{
+  dest[0] = src[0], dest[1] = src[1], dest[2] = src[2];
+}
+
+
+static void ivec_Scale( ivec dest, real C, ivec src )
+{
+  dest[0] = (int)(C * src[0]);
+  dest[1] = (int)(C * src[1]);
+  dest[2] = (int)(C * src[2]);
+}
+
+
+static void ivec_Sum( ivec dest, ivec v1, ivec v2 )
+{
+  dest[0] = v1[0] + v2[0];
+  dest[1] = v1[1] + v2[1];
+  dest[2] = v1[2] + v2[2];
+}
 
 #endif
